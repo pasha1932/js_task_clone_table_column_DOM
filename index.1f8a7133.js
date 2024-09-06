8aeaf9517112c6a9de340bf31091d3db9a7de63d
Pavlo Pukalo <misterkatlet@gmail.com>
2024-09-06 13:31:34 +0300

    make build

---
+++ index.1f8a7133.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),t=0;t<e.rows.length;t++){var n=void 0;(n=0===t||t===e.rows.length-1?document.createElement("th"):document.createElement("td")).innerHTML=e.rows[t].cells[1].innerHTML,e.rows[t].cells[4].insertAdjacentElement("beforebegin",n)}
+//# sourceMappingURL=index.1f8a7133.js.map
